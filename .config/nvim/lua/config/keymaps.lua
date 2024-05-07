b932b59085480e24780d226690832ff1fdd6b45a
Jonathan Rodenas <jonathan.rodenas@bemyvega.com>
2024-05-07 11:34:21 +0200

    [Add] Added nvim config

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,16 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+vim.keymap.set("i", "jj", "<ESC>", { silent = true })
+vim.keymap.set("i", "jk", "<ESC>", { silent = true })
+vim.keymap.set("i", "kj", "<ESC>", { silent = true })
+vim.keymap.set("i", "kk", "<ESC>", { silent = true })
+vim.keymap.set("i", "jh", "<ESC>", { silent = true })
+-- Define options
+local opts = { silent = true }
+
+-- Define keymaps
+vim.keymap.set("n", "<leader>ti", ":!terraform init<CR>", opts)
+vim.keymap.set("n", "<leader>tv", ":!terraform validate<CR>", opts)
+vim.keymap.set("n", "<leader>tp", ":!terraform plan<CR>", opts)
+vim.keymap.set("n", "<leader>taa", ":!terraform apply -auto-approve<CR>", opts)
